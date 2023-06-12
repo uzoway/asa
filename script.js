0d0464f3d64f9c6d927d9d177a57282a3b19e020
Uzochukwu Okafor <uzochukwuokafor01@gmail.com>
2023-06-12 21:11:49 +0100

    feat(hero): Add animation & smooth scroll for the hero section

---
+++ script.js
@@ -0,0 +1,69 @@
+// Register scrolltrigger plugin
+gsap.registerPlugin(ScrollTrigger);
+
+// initialize and set up Lenis smooth scroll
+const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+})
+
+function raf(time) {
+  lenis.raf(time);
+  ScrollTrigger.update();
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+function init() {
+    // Hero animation using matchmedia
+    let mm = gsap.matchMedia();
+
+    mm.add({ isDesktop: "(min-width: 280px) and (prefers-reduced-motion: no-preference)"}, (context) => {
+        let { isDesktop } = context.conditions;
+
+        let heroTl = gsap.timeline()
+        .to(".preloader__block", {
+            y: isDesktop ? "100vh" : "0",
+            stagger: {
+                each: 0.1,
+                from: "end"
+            },
+            delay: 1.5,
+            ease: "expo.out",
+            duration: 1.6
+        })
+        .to(".hero__navigation--logoImage", {
+            scale: 1,
+            transformOrigin: "0 0",
+            duration: 2,
+            ease: "expo.out",
+        }, "<0.4")
+        .from(".navigation__list", { 
+            y: isDesktop ?  "-4rem" : "0", 
+            opacity: isDesktop ? "0" : "1",
+            ease: "expo.out", 
+            duration: 1.2
+        }, "-=1.1")
+        .from(".hero__heading--text", { 
+            y: isDesktop ?  "-5vh" : "0", 
+            ease: "expo.out", 
+            duration: 1.2 
+        }, "-=1")
+        .from(".hero__videoPreview--paragraph, .hero__videoPreview--image", { 
+            y: isDesktop ?  "5vh" : "0",
+            opacity: isDesktop ? "0" : "1", 
+            ease: "expo.out", 
+            duration: 1.2 
+        }, "-=1")
+    })
+}
+
+
+
+
+
+
+window.addEventListener("load", init);
+
+
